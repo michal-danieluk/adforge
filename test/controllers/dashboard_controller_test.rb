ac55f7204bc5577c72a87abdc577998a9a6202ed
Michal <michal.danieluk@gmail.com>
2025-12-10 23:47:34 +0100

    Fix dashboard controller test with authentication

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get dashboard_index_url
+  setup do
+    @user = users(:one)
+    login_as(@user)
+  end
+
+  test "should get dashboard" do
+    get dashboard_url
     assert_response :success
+    assert_select "h1", "Dashboard"
+  end
+
+  test "should show brands count" do
+    get dashboard_url
+    assert_response :success
+    assert_select ".text-3xl", text: /\d+/  # Check for count numbers
   end
 end
